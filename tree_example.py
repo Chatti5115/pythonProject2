2fa652e9822e7f56473d516c0be32adeb415d169
Chatti5115 <yujink8114@gmail.com>
2021-08-27 22:50:09 +0900

    강의내용

--- tree_example.py
+++ tree_example.py
@@ -25,7 +25,6 @@ class NodeMgmt:
                     self.current_node.right = Node(value)
                     break
 
-
     def search(self, value):
         self.current_node = self.head
         while self.current_node:
@@ -37,45 +36,73 @@ class NodeMgmt:
                 self.current_node = self.current_node.right
         return False
 
-# head = Node(1)
-# BST = NodeMgmt(head)
-# BST.insert(2)
+    def delete(self, value):
+        # 삭제할 노드 탐색
+        searched = False
+        self.current_node = self.head
+        self.parent = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent = self.current_node
+                self.current_node = self.current_node.right
 
+        if searched == False:
+            return False
 
-head = Node(1)
-BST = NodeMgmt(head)
-BST.insert(2)
-BST.insert(3)
-BST.insert(0)
-BST.insert(4)
-BST.insert(8)
+            # case1
+        if self.current_node.left == None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = None
+            else:
+                self.parent.right = None
 
-print(BST)
+        # case2
+        elif self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.left
+            else:
+                self.parent.right = self.current_node.left
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.right
+            else:
+                self.parent.right = self.current_node.right
 
-BST.search(-1)
+                # case 3
+        elif self.current_node.left != None and self.current_node.right != None:
+            # case3-1
+            if value < self.parent.value:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.left = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.change_node.left
+            # case 3-2
+            else:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.right = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.current_node.left
 
-
-# def delete(self, value):
-searched = False
-self.current_node = self.head
-self.parent = self.head
-while self.current_node:
-    if self.current_node.value == value:
-        searched = True
-        break
-    elif value < self.current_node.value:
-        self.parent = self.current_node
-        self.current_node = self.current_node.left
-    else:
-        self.parent = self.current_node
-        self.current_node = self.current_node.right
-
-if searched == False:
-    return False
-
-if  self.current_node.left == None and self.current_node.right == None:
-    if value < self.parent.value:
-        self.parent.left = None
-    else:
-        self.parent.right = None
-    del self.current_node
+        return True
