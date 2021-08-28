5fec8673e362c752f9fea3fa2c512781ac68dcc9
Chatti5115 <yujink8114@gmail.com>
2021-08-28 18:19:41 +0900

    강의내용

--- heap_example.py
+++ heap_example.py
@@ -1,12 +1,12 @@
-class Heap:
-    def __init__(self, data):
-        self.heap_array = list()
-        self.heap_array.append(None)
-        self.heap_array.append(data)
-
-
-heap = Heap(1)
-heap.heap_arrayR
+# class Heap:
+#     def __init__(self, data):
+#         self.heap_array = list()
+#         self.heap_array.append(None)
+#         self.heap_array.append(data)
+#
+#
+# heap = Heap(1)
+# print(heap.heap_arrayR)
 
 
 class Heap:
@@ -32,4 +32,39 @@ heap.insert(8)
 heap.insert(5)
 heap.insert(4)
 heap.insert(20)
-heap.heap_array
+print(heap.heap_array)
+
+
+class Heap:
+    def __init__(self, data):
+        self.heap_array = list()
+        self.heap_array.append(None)
+        self.heap_array.append(data)
+
+    def move_up(self, inserted_idx):
+        if inserted_idx <= 1:
+            return False
+
+        parent_idx = inserted_idx // 2
+        if self.heap_array[inserted_idx] > self.heap_array[parent_idx]:
+            return True
+        else:
+            return False
+
+    def insert(self, data):
+        if len(self.heap_array) == 0:
+            self.heap_array.append(None)
+            self.heap_array.append(data)
+            return True
+
+        self.heap_array.append(data)
+
+        inserted_idx = len(self.heap_array) - 1
+
+        while self.move_up(inserted_idx):
+            parent_idx = inserted_idx // 2
+            self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[
+                inserted_idx]
+            inserted_idx = parent_idx
+
+        return True
